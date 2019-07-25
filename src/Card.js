5f80dd89a798def5c23eae9ecca1fa61d858205a
Sunny Chanthavong <sudochanth@gmail.com>
2019-07-25 15:13:28 -0600

    adding borders

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const Card = ( {name, email, id}) => {
     return (
-        <div>
+        <div style={{border: "3px solid black"}}>
             <img alt='robots' src={`https://robohash.org/${id}?200x200`} />
             <div>
                 <h2>{name}</h2>
--- src/CardList.js
+++ src/CardList.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {robots} from './robots';
+import './CardList.css';
 import Card from './Card'
 
 const CardList = ( { robots } ) => {
     return (
-        <div>
+        <div className="card-list">
         {
             robots.map((robot, i) => {
                 return <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
